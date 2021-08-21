0d146bbb9606a546d4835c7b57d010e0057a2d8f
xlollipopx <anton.stelmax@bk.ru>
2021-08-21 12:55:12 +0300

    task3

---
+++ internship/build.sbt
@@ -0,0 +1,5 @@
+name := "internship"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ internship/src/main/scala/basics/DataStructures.scala
@@ -0,0 +1,32 @@
+package basics
+
+import scala.collection.immutable.ListMap
+import scala.runtime.Nothing$
+
+object DataStructures {
+
+    def allSubsetsOfSizeN[A](set: Set[A], n: Int): Set[Set[A]] = {
+      set match {
+        case set if n == 1 => set.map(x => Set(x))
+        case set if set.nonEmpty =>
+          allSubsetsOfSizeN(set.tail, n - 1).map(x => x ++ Set(set.head)) ++ allSubsetsOfSizeN(set.tail, n)
+        case _ => Set.empty
+      }
+    }
+
+
+  def sortConsideringEqualValues[T](map: Map[T, Int]): List[(Set[T], Int)] = {
+    val sorted = ListMap(map.toSeq.sortWith(_._2 < _._2):_*)
+    sorted.toList.groupBy(_._2).view.mapValues(x => x.map(_._1).toSet).map{ case (x, y) => (y, x) }.toList
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val a = Set(1,2,3,4,5)
+    println(allSubsetsOfSizeN(a,3))
+    val m = Map("a" -> 1, "b" -> 2, "c" -> 4, "d" -> 1, "e" -> 0, "f" -> 2, "g" -> 2)
+    val res = sortConsideringEqualValues(m)
+    print(res)
+  }
+
+}
