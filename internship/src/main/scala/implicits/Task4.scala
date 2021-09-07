2f19bbf440fa184b88965fa213ddc62a539e629f
xlollipopx <anton.stelmax@bk.ru>
2021-09-07 14:59:44 +0300

    t4

---
+++ internship/src/main/scala/implicits/Task4.scala
@@ -0,0 +1,100 @@
+package implicits
+
+
+  object Task4 {
+    object Exercise4 {
+
+      trait Summable[T] {
+        def plus(a: T, b: T): T
+        def zero(): T
+      }
+
+
+      def +[T](a: T, b: T)(implicit summable: Summable[T]): T = {
+        summable.plus(a, b)
+      }
+
+      def empty[T]()(implicit summable: Summable[T]): T = {
+        summable.zero()
+      }
+
+      def fold[ F[_]: Foldable, T: Summable](seq: F[T], s: T, f: (T, T) => T )
+                                            (implicit summable: Summable[T], foldable: Foldable[F]): T ={
+        foldable.foldLeft(seq, s)(f)
+      }
+
+      object summableSyntax {
+
+        implicit class SummableOps[F[T]: Foldable, T: Summable](seq: F[T]) {
+          def genericSum(s: T) (f: (T, T) => T): T = {
+            Exercise4.fold(seq, s, f)
+          }
+        }
+      }
+
+      object summableInstances {
+
+        implicit def numericAdd[T: Numeric](implicit num: Numeric[T]): Summable[T] = new Summable[T] {
+          import num._
+          def plus(a: T, b: T): T = a + b
+          def zero(): T = num.zero
+        }
+
+        implicit def setAdd[S](): Summable[Set[S]] = new Summable[Set[S]] {
+          def plus(a: Set[S], b: Set[S]): Set[S] = a ++ b
+          def zero():  Set[S] = Set.empty[S]
+        }
+
+        implicit def listAdd[S](): Summable[List[S]] = new Summable[List[S]] {
+          def plus(a: List[S], b: List[S]): List[S] = a ++ b
+          def zero():  List[S] = List.empty[S]
+        }
+      }
+
+      case class Triple[T](
+                            v1: T,
+                            v2: T,
+                            v3: T,
+                          )
+
+      trait Foldable[F[_]] {
+        def foldLeft[T, S](ft: F[T], s: S)(f: (S, T) => S): S
+      }
+
+
+      object foldInstances{
+        implicit val optionFoldable: Foldable[Option] = new Foldable[Option] {
+          override def foldLeft[T, S](ft: Option[T], s: S)(f: (S, T) => S): S =
+            ft match {
+              case None => s
+              case Some(t) => f(s, t)
+            }
+        }
+
+        implicit val listFoldable: Foldable[List] = new Foldable[List] {
+          override def foldLeft[T, S](ft: List[T], s: S)(f: (S, T) => S): S = {
+            ft.foldLeft(s)(f)
+          }
+        }
+
+        implicit val tripleFoldable: Foldable[Triple] = new Foldable[Triple] {
+          override def foldLeft[T, S](ft: Triple[T], s: S)(f: (S, T) => S): S = {
+            val list = List(ft.v1, ft.v2, ft.v2)
+            list.foldLeft(s)(f)
+          }
+        }
+      }
+
+
+
+  }
+
+    def main(args: Array[String]): Unit = {
+      import Exercise4.summableInstances._
+      import Exercise4.summableSyntax._
+      import Exercise4.foldInstances._
+      println(List(1,2,3).genericSum(0)((a: Int, b: Int) => a + b))
+    }
+
+
+  }
--- internship/src/main/scala/implicits/implicits.scala
+++ internship/src/main/scala/implicits/implicits.scala
@@ -94,100 +94,6 @@ object implicits {
 
     }
 
-  object Exercise4 {
-
-    /*
-    Generic foldLeft!
-    F[_] - type constructor with a single type argument, like List[T], Option[T], etc.
-    Types which are parameterized using type constructors called higher-kinded types (HKT)
-    Foldable here is a HKT
-     */
-
-    trait Summable[T] {
-      def plus(a: T, b: T): T
-      def zero(): T
-    }
-
-
-    def +[T](a: T, b: T)(implicit summable: Summable[T]): T = {
-      summable.plus(a, b)
-    }
-
-    def empty[T]()(implicit summable: Summable[T]): T = {
-      summable.zero()
-    }
-
-    def fold[ F[_]: Foldable, T: Summable](seq: F[T], s: T, f: (T, T) => T )
-                                          (implicit summable: Summable[T], foldable: Foldable[F]): T ={
-      foldable.foldLeft(seq, s)(f)
-    }
-
-    object summableSyntax {
-
-      implicit class SummableOps[F[T]: Foldable, T: Summable](seq: F[T]) {
-        def genericSum(s: T) (f: (T, T) => T): T = {
-          Exercise4.fold(seq, s, f)
-        }
-      }
-    }
-
-    object summableInstances {
-
-      implicit def numericAdd[T: Numeric](implicit num: Numeric[T]): Summable[T] = new Summable[T] {
-        import num._
-        def plus(a: T, b: T): T = a + b
-        def zero(): T = num.zero
-      }
-
-      implicit def setAdd[S](): Summable[Set[S]] = new Summable[Set[S]] {
-        def plus(a: Set[S], b: Set[S]): Set[S] = a ++ b
-        def zero():  Set[S] = Set.empty[S]
-      }
-
-      implicit def listAdd[S](): Summable[List[S]] = new Summable[List[S]] {
-        def plus(a: List[S], b: List[S]): List[S] = a ++ b
-        def zero():  List[S] = List.empty[S]
-      }
-    }
-
-    case class Triple[T](
-                          v1: T,
-                          v2: T,
-                          v3: T,
-                        )
-
-    trait Foldable[F[_]] {
-      def foldLeft[T, S](ft: F[T], s: S)(f: (S, T) => S): S
-    }
-
-
-    object foldInstances{
-      implicit val optionFoldable: Foldable[Option] = new Foldable[Option] {
-        override def foldLeft[T, S](ft: Option[T], s: S)(f: (S, T) => S): S =
-          ft match {
-            case None => s
-            case Some(t) => f(s, t)
-          }
-      }
-      implicit val listFoldable: Foldable[List] = new Foldable[List] {
-        override def foldLeft[T, S](ft: List[T], s: S)(f: (S, T) => S): S = {
-          ft.foldLeft(s)(f)
-        }
-      }
-
-
-
-      implicit val tripleFoldable: Foldable[Triple] = new Foldable[Triple] {
-        override def foldLeft[T, S](ft: Triple[T], s: S)(f: (S, T) => S): S = {
-          val list = List(ft.v1, ft.v2, ft.v2)
-          list.foldLeft(s)(f)
-        }
-      }
-
-
-    }
-
-  }
 
     def main(args: Array[String]): Unit = {
       import Task1.instances._
@@ -196,23 +102,18 @@ object implicits {
       import Exercise3.instances._
       import Exercise3.hde._
       import Exercise3.CustomNumber
-      import Exercise4.summableInstances._
-      import Exercise4.summableSyntax._
-      import Exercise4.foldInstances._
 
-//      42.show
-//      println("hello!".show)
-//      println(1, 313.show)
-//      println(Seq("I", "am", "a", "ghost").show)
-//      println(Seq(Seq(1, 2, 3), Seq(2)).show)
-//      println(Human("Denis").show)
-//      println(List(1,2,3).sumAll())
-//      println(List(CustomNumber(1), CustomNumber(2.23f),CustomNumber(2)).sumAll())
-//      println(secondBiggestValue(Seq(HDEYears(1), HDEYears(2), HDEYears(4),HDEYears(5),HDEYears(5))))
-//      println(secondBiggestValue(Seq()))
-//      print(List.empty[Int].sumAll())
-
-       println(List(1,2,3).genericSum(0)((a: Int, b: Int) => a + b))
+      42.show
+      println("hello!".show)
+      println(1, 313.show)
+      println(Seq("I", "am", "a", "ghost").show)
+      println(Seq(Seq(1, 2, 3), Seq(2)).show)
+      println(Human("Denis").show)
+      println(List(1,2,3).sumAll())
+      println(List(CustomNumber(1), CustomNumber(2.23f),CustomNumber(2)).sumAll())
+      println(secondBiggestValue(Seq(HDEYears(1), HDEYears(2), HDEYears(4),HDEYears(5),HDEYears(5))))
+      println(secondBiggestValue(Seq()))
+      print(List.empty[Int].sumAll())
 
     }
 
